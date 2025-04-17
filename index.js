ac9bd21c735b3d133d3417674434cba37415d14a
toson11 <iamtoson@outlook.com>
2025-04-17 23:41:13 +0800

    feat: 添加规则

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: true,
+  printWidth: 100,
+  trailingComma: "all",
+  useTabs: false,
+  htmlWhitespaceSensitivity: "strict",
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+};
